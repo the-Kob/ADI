8bbf9b71b87b91e904fadb7b9b12dcadb39291a7
Guilherme Pereira <gcn.pereira@campus.fct.unl.pt>
2023-03-24 13:18:25 +0000

    Auto stash before merge of "main" and "origin/main"

--- Labs/2/utils/policy-iteration.py
+++ Labs/2/utils/policy-iteration.py
@@ -1,12 +1,12 @@
 import numpy as np
+
 # X -> States
 # A -> Actions
 # P -> Transition probabilities
 
 # M = (X, A, P, c, 0.99)
 
-
-def policy_iteration(M, eps):
+def policy_iteration(M):
     X = M[0]
     A = M[1]
     P = M[2]
@@ -21,33 +21,35 @@ def policy_iteration(M, eps):
     while not quit:
         # Auxiliary array to store intermediate values
         Q = np.zeros((len(X), len(A)))
-
+        
         # Policy evaluation
         cpi = np.sum(c * pol, axis=1, keepdims=True)
         Ppi = pol[:, 0, None] * P[0]
-
-        J = np.linalg.inv(np.eye(len(X))- gamma * Ppi).dot(cpi)
-
-        # Compute Q values
+        
+        for a in range(1, len(A)):
+            Ppi += pol[:, a, None] * P[a]
+            
+        J = np.linalg.inv(np.eye(len(X)) - gamma * Ppi).dot(cpi)
+        
+        # Compute Q-values
         for a in range(len(A)):
-            Q[:, a, None] = c[:, a, None] + gamma * P[a].dot(J)
+            Q[:, a, None]  = c[:, a, None] + gamma * P[a].dot(J)
         
         # Compute greedy policy
         Qmin = np.min(Q, axis=1, keepdims=True)
-
-        pnew = np.isclose(Q, Qmin, atol=1e-8, rtol=1e-8).astype(int)
+        
+        pnew = np.isclose(Q, Qmin, atol=le-8, rtol=le-8).astype(int)
         pnew = pnew / pnew.sum(axis=1, keepdims=True)
-
+        
         # Compute stopping condition
         quit = (pol == pnew).all()
-
+        
         # Update
         pol = pnew
         niter += 1
         
-        
     print(f'Done after {niter} iterations.')
     return np.round(pol, 3)
-
+    
 # pol = policy_iteration(M)
 # print(pol)
